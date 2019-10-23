bcbd1a6b7ae6ca116a214afb28eb6e64d6f069be
gitdavemattson <54650817+gitdavemattson@users.noreply.github.com>
2019-10-22 22:09:10 -0600

    Week 9 - p5.js

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup(){
+  createCanvas(400, 400);
+}
+
+function draw(){
+  background(256, 100, 256);
+  rect(163,200,75,200);
+  ellipse(90, 200, 50, 75);
+  ellipse(310, 200, 50, 75);
+  rect(100, 50, 200, 150)
+  ellipse(200, 200, 200, 250);
+  circle(150, 200, 75);
+  circle(250, 200, 75);
+  triangle(200, 190, 215, 250, 185, 250);
+  line(150, 275, 250, 275);
+  stroke(0, 256, 256);
+  point(90,200);
+  point(310, 200);
+  textSize(20);
+  text('"DaveMM Basic Shape Boy Ed."', 10, 25);
+  text('By: David M. Mattson', 200, 375);
+
+}
